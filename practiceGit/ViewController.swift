0a84a736ac324242430f31c6819456ff3eb584e5
jamad <jamad2001@gmail.com>
2016-12-27 20:04:26 +0200

    test

--- practiceGit/ViewController.swift
+++ practiceGit/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label=UILabel()
+        view.addSubview(label)
+        view.backgroundColor=UIColor.cyan
+        
+        label.text="helloWorld"
+        label.textAlignment=NSTextAlignment.center
+        label.frame.size=CGSize(width: 100, height: 50) // size first
+        label.center=CGPoint(x: view.bounds.width/2, y: view.bounds.height/2) // position after size
+        
+        label.backgroundColor=UIColor.orange    // backcolor
+        label.textColor=UIColor.white           // forecolor
+        label.shadowColor=UIColor.blue          // shadowcolor
+        
+        label.layer.masksToBounds=true          // round corner on
+        label.layer.cornerRadius=20             // round corner radius
     }
 
     override func didReceiveMemoryWarning() {
